052bb8a67ba66e83fd318e535819b8b80df16f9f
copyrightpoiiiii <barcelonakeyfc@qq.com>
2019-10-09 09:05:12 +0800

    add a new api but do not complete

---
+++ children_study/src/org/Dao/QueryInfo.java
@@ -0,0 +1,40 @@
+package org.Dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class QueryInfo {
+	String drv = "com.mysql.cj.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/children_study?useSSL=false&serverTimezone=UTC";
+	String usr = "root";
+	String pwd = "949462395";
+
+	public ArrayList<String> Query(int grade) {
+		/*
+		 * 返回一个包含30条String的数组
+		 * 每道题目为一个String
+		 * 保证难度为grade
+		 */
+		ArrayList<String> problem = new ArrayList<String>();
+		String sql = "SELECT * FROM `problem` WHERE  level = "+grade+""+" ORDER BY Rand() LIMIT 30";
+		try {
+			Class.forName(drv);
+			Connection connect = DriverManager.getConnection(url, usr, pwd);
+			Statement stm = connect.createStatement();
+			ResultSet rs = stm.executeQuery(sql);
+			while(rs.next()) {
+				problem.add(rs.getString(""));
+			}
+			rs.close();
+			stm.close();
+			connect.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return problem;
+	}
+}
